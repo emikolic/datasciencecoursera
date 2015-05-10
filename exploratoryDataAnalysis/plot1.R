888240e682e39eb4cfc842c4c572ceabbb713bc7
Emina Mikolic <emina.mikolic@gmail.com>
2015-05-10 20:49:40 +0200

    Script to draw plot 1

--- exploratoryDataAnalysis/plot1.R
+++ exploratoryDataAnalysis/plot1.R
@@ -1,13 +1,13 @@
 plot1<-function()
 {
- ##  reading txt file with data
-data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+## in order to run this script you have to have a txt dataset in your working directory
+##  reading txt file with data
+data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
 
 ## getting only data for 2007.02.01 and 2007.02.02
 data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_full)
 
 ## convert dates
 datetime <- paste(as.Date(data$Date), data$Time)
